fa206c9de77d4f1c2078f1d8ef68a6924d619c67
zew212 <zew212@nyu.edu>
2025-09-16 15:44:02 -0400

    added a couple shapes and change background and object color

--- week2/sketch.js
+++ week2/sketch.js
@@ -2,9 +2,22 @@
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
-}
+  background (150,210,227); // background color (rgb)
+
+
+};
 
 function draw() {
-  
-}
+  circle(200,200,150) ; // runs a loop after setup: 
+  // x position, y position, diameter
+  triangle(9,150,200,9,200,125);
+  //triangle(125, 200, 9, 200, 200, 9)
+  ellipse(250, 80, 10, 20)
+  fill(250, 80, 10, 20);
+  ellipse(250, 300, 200, 20);
+  line(250, 90, 250, 290); // connecting the ellipses
+
+};
+
+rect(100,80,100,10);
+
