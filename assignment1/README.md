7c74b6417f3e56ace85d88256c78f1744f761937
zew212 <zew212@nyu.edu>
2025-10-07 12:20:34 -0400

    fixing menu

--- assignment1/README.md
+++ assignment1/README.md
@@ -1,61 +1,10 @@
-# Assignment 1
+# H1 Assignment 1
 
----
+> “As a result, instead of optimizing our machines for humanity—or even the benefit of some particular group—we are optimizing humans for machinery.” Douglas Rushkoff pg. 15
 
-This is a README file that can be used to describe and document your assignment.
+*This quote stood out to me because it brakes down our past relationship with machinery and how even through this new technology, we still are conforming ourselves into the mold of the machine. He compares the usurption of labor by machines to the devaluing of human thought in this digital age. It strengthens a hierarchical system that leaves the majority of people in the hands of the few.* 
 
-Markdown Cheatsheet (from [https://www.markdownguide.org/cheat-sheet/](https://www.markdownguide.org/cheat-sheet/)):
+> “Once these skills and machines started becoming essential to industry and government, they were reclassified as jobs for male experts. Women  were  then  deskilled  and  downgraded,  emerging  “as  the operators of machinery, [who] do not have, nor are expected to have, knowledge of the inner workings of those machines.” Jathan Sadowski pg. 39
 
----
+*I picked this quote because of how it tied to some of the issues referenced in the Rushkoff text. It speaks to the power structure within machinery and the development of productivity and value. “Technology” is only seen as futuristic and when it becomes common, the knowledge needed to use the machine can become compartmentalized and the labor of the actual humans working on it becomes less valuable. Through this, we can see how different people are valued within society based on the believed amount of capital they can contribute.*
 
-# Heading1
-## Heading2
-### Heading3
-#### Heading4
-##### Heading5
-###### Heading6
-
-**bold text**
-
-*italicized text*
-
-~~strikethrough text~~
-
-Ordered List:
-1. First item
-2. Second item
-3. Third item
-
-Unordered List:
-- First item
-- Second item
-- Third item
-
-`short code block`
-
-```
-extended code block
-fun() {
-  return 0
-}
-```
-
-Link:  
-[linked text](https://www.example.com)
-
-
-Image with url:  
-![image description](https://dm-gy-6063-2024f-b.github.io/assets/homework/02/clark-espaco-modulado-00.jpg)
-
-
-Image on repo:  
-![image description](./file-name.jpg)
-
-
-To start a new line, add two spaces at the end of a line, like this:  
-this is a new line.
-
-
-To start a new paragraph, leave an empty line between two lines of text.
-
-This is a new paragraph.
--- assignment1/sketch.js
+++ assignment1/sketch.js
@@ -1,8 +1,118 @@
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
+  // make the canvas the size of the window
+  background(234,232,224);
+  // set the background color to a light gray
+  
 }
 
 function draw() {
+
+fill("rgba(190, 8, 120, 0.2)");
+beginShape();
+// vertex(x, y)
+vertex(windowWidth/2, 0);
+// top left point (was at 240)
+vertex(windowWidth/3, 150);
+// bottom left point
+vertex(windowWidth/1.8, 205);
+// bottom right point
+vertex(windowWidth/1.4, 50);
+// top right point
+endShape();
+
+noStroke()
+fill("rgba(240, 219, 32, 1)");
+circle(windowWidth/2.1,500,18);
+// circle(x, y, diameter)
+// the higher the x value, the more to the right
+// the higher the y value, the more to the bottom
+
+fill("rgba(17, 227, 216, 1)");
+beginShape();
+// vertex(x, y)
+vertex(windowWidth/7.5, 385);
+// top left point
+vertex(windowWidth/7.5, 395);
+// bottom left point
+vertex(windowWidth/1.43, 410);
+// bottom right point
+vertex(windowWidth/1.43, 400);
+// top right point
+endShape();
+
+strokeWeight(weight=2);
+fill("rgba(85, 107, 64, 0.5)");
+//rect(100, 375, 50, 50)
+// (x, y, width, height)
+
+
+
+fill("rgba(56, 14, 224, 0.5)");
+//rect(70, 400, 300, 10);
+// (x, y, width, height)
+// x and y are the coordinates of the top left corner
+// can rectatngles use different y values to create a slant?
+
+
+fill("rgba(87, 20, 94, 0.5)");
+beginShape();
+// vertex(x, y)
+vertex(windowWidth/3, 300);
+// top left point
+vertex(windowWidth/2.5, 420);
+// bottom left point
+vertex(windowWidth/2, 420);
+// bottom right point
+vertex(windowWidth/1.7, 310);
+// top point
+endShape();
+
+
+fill("rgba(148, 165, 92, 0.5)");
+beginShape();
+// vertex(x, y)
+vertex(windowWidth/3.9, 455);
+// top left point
+vertex(windowWidth/3.6, 480);
+// bottom left point
+vertex(windowWidth/2.53, 455);
+// bottom right point
+vertex(windowWidth/2.7, 430);
+// top right point
+endShape();
+
+fill("rgba(25, 99, 99, 0.5)");
+beginShape();
+// vertex(x, y)
+vertex(windowWidth/2.65, 465);
+// top left point
+vertex(windowWidth/2.58, 480);
+// bottom left point
+vertex(windowWidth/2.23, 470);
+// bottom right point
+vertex(windowWidth/2.28, 455);
+// top right point
+endShape();
+
+fill("rgba(25, 99, 99, 0.5)");
+beginShape();
+// vertex(x, y)
+vertex(windowWidth/2.3, 475);
+// top left point
+vertex(windowWidth/2.3, 485);
+// bottom left point
+vertex(windowWidth/1.61, 490);
+// bottom right point
+vertex(windowWidth/1.6, 480);
+// top right point
+endShape();
+
+fill("rgba(148, 165, 92, 1)");
+rect(windowWidth/6, 365, 50, 50);
+// (x, y, width, height)
+// square in the bottom right corner
+// opposite of the first square
 
 }
