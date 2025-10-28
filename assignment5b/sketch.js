b8105a7d86e6e48ef9f2d190cffca3e711d324b9
zew212 <zew212@nyu.edu>
2025-10-28 19:19:14 -0400

    working on assignment 5

--- assignment5b/sketch.js
+++ assignment5b/sketch.js
@@ -1,10 +1,52 @@
-
+let drunkX= 200;
+let drunkY= 200;
+let drunkRange= 5;
+let drunkDiameter= 5;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
+  background (0);
   
+
+// PUMPKIN
+  fill ('orange');
+  ellipse(200,200,250,200);
+// RINGS
+  noStroke();
+  fill ('rgba(177, 110, 10, 0.5)');
+  ellipse(200,200,200,200);
+  // ellipse(x, y, width, height)
+  fill ('orange');
+  ellipse(200,200,150,200);
+  fill ('rgba(177, 110, 10, 0.5)');
+  ellipse(200,200,100,200);
+  fill ('orange');
+  ellipse(200,200,50,200);
+
+
+// STEM
+  fill ('rgba(111, 77, 53, 1)');
+  beginShape();
+  vertex(190,120);
+  vertex(200,120);
+  vertex(230,70);
+  vertex(210,50);
+  endShape();
+// LEAF 
+  fill ('green');
+  beginShape();
+  vertex(170,70);
+  vertex(170,100);
+  vertex(150,100);
+  vertex(190,120);
+  endShape();
+
 }
 
 function draw() {
-  
+
+  fill (0);
+  drunkX = drunkX + random(-drunkRange, drunkRange);
+  drunkY = drunkY + random(-drunkRange, drunkRange);
+  circle(drunkX, drunkY, drunkDiameter);
 }
--- assignment5c/sketch.js
+++ assignment5c/sketch.js
@@ -1,10 +1,43 @@
+let circleX, circleY;
+let circleDiameter = 10;
+let thetaX = 0;
+let thetaY = 0;
+let radiusX = 90;
+let radiusY = 20;
 
+let speedX = 3;
+let speedY = 3;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
   
 }
 
 function draw() {
-  
+  background(255);
+
+// CAULDRON
+  stroke(0);
+  fill(100);
+  ellipse(200,300,180,150);
+  ellipse(200,255,200,50);
+  ellipse(200,250,200,50);
+  fill('rgba(12, 72, 35, 1)');
+  ellipse(200,250,190,45);
+
+// BUBBLE
+  fill(100);
+	circleX = width/2 + cos(radians(thetaX))*radiusX; // set an x value in relation to the cos() value of theta at a polar position of width/2, amplified by a radius variable
+	circleY = 250 + sin(radians(thetaY))*radiusY;
+
+  noStroke();
+  fill('rgba(122, 147, 110, 1)');
+	circle(circleX,circleY, circleDiameter);
+	thetaX+=speedX;
+	thetaY+=speedY;
+
+  // STIRRING STICK
+  fill('rgba(78, 27, 27, 1)');
+  rect(circleX,150,5,100);
+
 }
