0a322857f64051f7032ff32945d7ef59c636af34
zew212 <zew212@nyu.edu>
2025-10-28 19:58:57 -0400

    working on eyes

--- assignment5a/sketch.js
+++ assignment5a/sketch.js
@@ -6,34 +6,51 @@ function setup() {
   createCanvas(400, 400);
   
 }
-
+ 
 function draw() {
-  background ('#3d12e8ff');
+  background (0);
   translate (width / 4, height / 4); // I used the code from the 
   // class lesson and now my function is built around this translate
-  noStroke();
-  fill ('#FF5733');
+
+
   let diam = noise(noisePosition);
   diam = map(diam, 0, 1, 0, height);
-    circle(0,0,diam);
-  fill ('#3357FF');
-    circle(-3, -4, diam*0.9);
-  fill ('#33FF57');
-    circle(width/2,-3,diam*0.75);
-  fill ('#1dbf8cff');
-    circle(width / 2,50,diam*0.5);
-  fill ('#f37515ff');
-    circle(4,3,diam*0.6);
-  fill ('#ad6a44ff');
-    circle(150,250,diam);
-  fill ('#f0760cff');
-    circle(250,200,diam*0.75);
-  fill ('#7f4f24ff');
-    circle(250,200,diam*0.5);
-  fill ('#c1440eff');
-    circle(-50,150,diam*0.4);
-  fill ('#e6b89cff');
-    circle(0,150,diam*0.18);
+  // if the circles (pupils) are centered at the same point as the eclipses (eyes),
+  // the pupil will never be shown out of the eye
+  fill (255);
+    ellipse(width/2,-3,diam*1.0, diam*0.6);
+  fill ('#d80505ff');
+    circle(width/ 2,-3,diam*0.2);
+    fill (255);
+    ellipse(300,50,diam*0.7, diam*0.4);
+  fill ('#d80505ff');
+    circle(300,50,diam*0.2);
+  fill (255);
+    ellipse(250,200,diam*0.8, diam*0.5);
+  fill ('#d80505ff');
+    circle(250,200,diam*0.4);
+  fill (255);
+    ellipse(0,0,diam*1.2, diam*0.7);
+  fill ('#d80505ff');
+    circle(0,0,diam*0.4);
+  fill (255);
+    ellipse(50,200,diam*0.9, diam*0.5);
+  fill ('#d80505ff');
+    circle(50,200,diam*0.3);
+  fill (255);
+    ellipse(180,140,diam*0.6, diam*0.3);
+  fill ('#d80505ff');
+    circle(180,140,diam*0.1);
+  fill (255);
+    ellipse(-60,150,diam*0.5, diam*0.2);
+  fill ('#d80505ff');
+    circle(-60,150,diam*0.08);
+
+
+
+
+
+
 
   noisePosition = noisePosition + speed;
    
